89f137b9bba837bf5cfe217c41fd1069e1a1c8c3
Branko Djurkovic <brankko@gmail.com>
2016-04-13 17:56:32 +0200

    yo angular

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name touristifyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the touristifyApp
+ */
+angular.module('touristifyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name touristifyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the touristifyApp
+ */
+angular.module('touristifyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
