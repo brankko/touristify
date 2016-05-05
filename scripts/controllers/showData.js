2257ef8b2384a2a1b688670900af1934515399bd
Your Name <you@example.com>
2016-05-05 17:31:38 +0200

    calculated avg age

--- scripts/controllers/showData.js
+++ scripts/controllers/showData.js
@@ -7,12 +7,29 @@
  * # ShowDataCtrl
  * Controller of the touristifyApp
  */
-angular.module('touristifyApp')
-  .controller('ShowDataCtrl', function ($scope, $http) {
+ angular.module('touristifyApp')
+ .controller('ShowDataCtrl', function ($scope, $http) {
 
-  	$scope.people = null;
-  	$http.get('data/people.json').success(function(data){
-  		$scope.people = data;
-  	});
+   $scope.people = null;
+   $http.get('data/people.json').success(function(data){
+    $scope.people = data;
+    var length = $scope.people;
+    var total =0;
+    var avarage ;
+
+
+
+    for (var i = 0; i < data.length; i++) {
+        total += data[i].Age;
+        avarage = total/(i+1);
     
+        console.log(avarage);
+    }
+
+
+
   });
+
+
+
+ });
