e1f90abc4158ef8b4c34f7cbd467a0d240447aa2
Branko Djurkovic <brankko@gmail.com>
2016-05-12 19:08:53 +0200

    slider range

--- scripts/controllers/showData.js
+++ scripts/controllers/showData.js
@@ -10,26 +10,43 @@
  angular.module('touristifyApp')
  .controller('ShowDataCtrl', function ($scope, $http) {
 
-   $scope.people = null;
-   $http.get('data/people2.json').success(function(data){
-    $scope.people = data;
-    var length = $scope.people;
-    var total =0;
-    var avarage ;
-    var counter=0;
-    var counter2=0;
-    $scope.lowLimit = 55;
-    $scope.highLimit = 51;
+    //slider code
+    $scope.slider = {
+      min: 18,
+      max: 24,
+      options: {
+        floor: 14,
+        ceil: 99,
+        onChange: function(){
+          $scope.count($scope.people);
+        }
+      }
+    };
+
+    $scope.people = null;
+    $http.get('data/people2.json').success(function(data){
+      $scope.people = data;
+      $scope.count(data);
+    });
+
+    $scope.count = function(data){
+      var length = $scope.people;
+      var total =0;
+      var avarage ;
+      var counter=0;
+      var counter2=0;
+    //    $scope.lowLimit = 55;
+    //    $scope.highLimit = 51;
 
     for (var i = 0; i < data.length; i++) {
 
       total += data[i].age;
       avarage = total/(i+1);
 
-      if( data[i].age >=  $scope.lowLimit ){
+      if( data[i].age >=  $scope.slider.max ){
         counter ++;
       }
-      if( data[i].age <=  $scope.highLimit ){
+      if( data[i].age <=  $scope.slider.min ){
         counter2 ++;
       }
     }
@@ -38,12 +55,6 @@
     $scope.counter2 = counter2;
 
     $scope.avarage= Math.round(avarage);
+  };
 
-    
-    //slider code
-    $scope.slider = {
-      value: 10
-    };
-  });
-   
- });
+});
