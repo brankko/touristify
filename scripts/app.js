5139d7a405ec77b66d07548448585cb66d0bb026
Branko Djurkovic <brankko@gmail.com>
2016-06-01 16:46:49 +0200

    adding new controller

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,11 @@ var touristifyApp = angular
         controller: 'PeopleCtrl',
         controllerAs: 'people'
       })
+      .when('/report', {
+        templateUrl: 'views/report.html',
+        controller: 'ReportCtrl',
+        controllerAs: 'report'
+      })
       .when('/show', {
         templateUrl: 'views/show.html',
         controller: 'ShowDataCtrl',
---
+++ scripts/controllers/report.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name touristifyApp.controller:ReportCtrl
+ * @description
+ * # ReportCtrl
+ * Controller of the touristifyApp
+ */
+touristifyApp.controller('ReportCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
